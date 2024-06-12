a9faa852629102f17f4a00266936980da571ad72
ChardJosephEstrera <96616697+ChardJosephEstrera@users.noreply.github.com>
2024-06-12 16:24:01 +0800

    Add files via upload

---
+++ My second website/dashboard.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="dashboard-style.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    
+      
+
+</head>
+<body>
+    
+
+<header class="nav">
+   <div class="greet"> 
+             <h2> Welcome to Console Hub</h2>
+        </div>
+
+    <nav class="navbar"> 
+     <a href="Home"> Home </a>
+     <a href="Consoles"> Consoles  </a>
+     <a href="About Us"> About Us </a>
+     <a href="Support"> Support </a>
+    </nav>
+    
+</header>
+<main>
+      
+    
+        <div class="top">
+         
+            <div class="box">
+                <h2> These are the top consoles:</h2>
+                <b> Playstation 5 </b> </br>
+                <p> <img src="playstation5.webp" height="250" width="">     </p>
+                <b> Nintendo Switch</b> </br>
+                <b> Steam Deck</b> </br>
+                <b> Xbox 360</b> </br>
+
+                
+
+                
+            </div>
+
+        </div>
+
+    </he>
+</main>
+ 
+
+
+ </div>
+
+
+
+    
+
+
+    
+</body>
+</html>
---
+++ My second website/login.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logins</title>
+    <link rel="stylesheet" href="style.css">
+
+
+
+</head>
+<body>
+<div class="wrapper">
+
+ <h1> LOGIN</h1>
+ <forms action="" method=""> </forms>
+
+    <div class="log-user">
+
+     
+
+     <input type="text" placeholder="username" required>
+     <label for="Username">  </label>
+        
+    </div>
+
+    <div class=" log-pass">
+      </br>
+     <input type="text" placeholder="Password" required>
+     <label for="Password">   </label>
+     
+     </div>
+
+    <div class="forgot-pass">
+        </br>
+        <label> <input type="checkbox"> Remember me</label>
+        <a href="#" > forgot Password  </a> 
+
+    </div>
+    
+    <div class="login-btn">   
+     <br/>
+     <button type="submit" class="btn">  Login </button>
+      
+
+     </div>
+
+
+
+    <div class="create-account"> 
+     </br>
+     <p>Don't have an account?<a href="#" > Sign Up </a> </p> 
+
+     
+    </div>
+
+
+
+
+
+
+
+
+    </div>
+
+    
+     </body>
+     </html>
